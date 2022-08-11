fb8f9bb99380e1fafc9b9b6b44aa184b805f2351
Christian Nieves <cnieves@google.com>
2022-08-11 18:54:02 +0000

    Missing vim func

--- config/.config/nvim/lua/lsp.lua
+++ config/.config/nvim/lua/lsp.lua
@@ -12,6 +12,11 @@ configs.ciderlsp = {
   },
 }
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 -- 2. Configure CMP
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 

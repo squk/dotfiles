3c5764912dc5cea7e9ea819fdaba512aeddb312c
Christian Nieves <cnieves@google.com>
2023-07-06 10:04:23 -0500

    slow notify FPS

--- vim/.vim/lua/plugins/nvim-notify.lua
+++ vim/.vim/lua/plugins/nvim-notify.lua
@@ -4,6 +4,9 @@ return {
 		local colors = require("catppuccin.palettes").get_palette()
 		require("notify").setup({
 			background_colour = colors.base,
+			fps = 10, -- default 30
+			stages = "slide", -- default fade_in_slide_out
+			timeout = 5000, -- default 5000
 		})
 		vim.notify = require("notify")
 	end,

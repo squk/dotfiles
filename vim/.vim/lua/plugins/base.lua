9c7f778344be1dbd9384004b9a023568bcaa59a1
Christian Nieves <cnieves@google.com>
2023-11-01 22:06:18 -0500

    firenvim config

--- vim/.vim/lua/plugins/base.lua
+++ vim/.vim/lua/plugins/base.lua
@@ -9,7 +9,7 @@ return {
 		"andweeb/presence.nvim",
 		cond = not use_google(),
 		config = function()
-			require("presence").setup()
+			require("presence").setup({ show_time = false })
 		end,
 	},
 	{
@@ -101,5 +101,43 @@ return {
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
 		ft = { "markdown" },
+	},
+	{
+		"glacambre/firenvim",
+
+		-- Lazy load firenvim
+		-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+		config = function()
+			vim.g.firenvim_config = {
+				localSettings = {
+					[ [[.*]] ] = {
+						cmdline = "firenvim",
+						priority = 0,
+						selector = 'textarea:not([readonly]):not([class="handsontableInput"]), div[role="textbox"]',
+						takeover = "always",
+					},
+					[ [[.*notion\.so.*]] ] = {
+						priority = 9,
+						takeover = "never",
+					},
+					[ [[.*docs\.google\.com.*]] ] = {
+						priority = 9,
+						takeover = "never",
+					},
+					[ [[google\.com.*]] ] = {
+						priority = 9,
+						takeover = "never",
+					},
+					[ [[twitch\.tv.*]] ] = {
+						priority = 9,
+						takeover = "never",
+					},
+				},
+			}
+		end,
+		lazy = not vim.g.started_by_firenvim,
+		build = function()
+			vim.fn["firenvim#install"](0)
+		end,
 	},
 }

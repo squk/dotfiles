2424cd19697bc827167b9f65cc8d1e4beccba99f
Christian Nieves <cnieves@google.com>
2023-11-10 16:58:02 -0600

    meh

--- vim/.vim/lua/plugins/themes-personal.lua
+++ vim/.vim/lua/plugins/themes-personal.lua
@@ -5,8 +5,7 @@ return {
 		"rebelot/kanagawa.nvim",
 		lazy = use_google(), -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
-		cond = not use_google(),
-		cond = false,
+		-- cond = not use_google(),
 		config = function()
 			-- vim.cmd("colorscheme kanagawa")
 		end,
@@ -28,86 +27,6 @@ return {
 		cond = not use_google(),
 		config = function()
 			-- vim.cmd("colorscheme bluloco")
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = not use_google(), -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		cond = use_google(),
-		config = function()
-			vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-			local colors = require("catppuccin.palettes").get_palette()
-
-			require("catppuccin").setup({
-				flavour = "macchiato",
-				integrations = {
-					cmp = true,
-					-- coc_nvim = false,
-					dashboard = true,
-					-- fern = false,
-					fidget = true,
-					gitgutter = true,
-					gitsigns = true,
-					-- hop = false,
-					-- illuminate = false,
-					-- leap = false,
-					-- lightspeed = false,
-					-- lsp_saga = false,
-					lsp_trouble = true,
-					mason = true,
-					markdown = true,
-					-- neogit = false,
-					-- neotest = false,
-					neotree = true,
-					notify = true,
-					-- nvimtree = true,
-					-- overseer = false,
-					-- pounce = false,
-					symbols_outline = true,
-					telescope = true,
-					treesitter = true,
-					treesitter_context = false,
-					-- ts_rainbow = false,
-					-- vim_sneak = false,
-					-- vimwiki = false,
-					which_key = true,
-
-					-- Special integrations, see https://github.com/catppuccin/nvim#special-integrations
-					dap = {
-						enabled = false,
-						enable_ui = false,
-					},
-					indent_blankline = {
-						enabled = true,
-						colored_indent_levels = false,
-					},
-					native_lsp = {
-						enabled = true,
-						virtual_text = {
-							errors = { "italic" },
-							hints = { "italic" },
-							warnings = { "italic" },
-							information = { "italic" },
-						},
-						underlines = {
-							errors = { "underline" },
-							hints = { "underline" },
-							warnings = { "underline" },
-							information = { "underline" },
-						},
-					},
-				},
-				custom_highlights = {
-					Identifier = { fg = colors.lavender },
-					Statement = { fg = colors.rosewater },
-					-- Identifier = { fg = colors.sapphire },
-					Function = { fg = colors.mauve },
-				},
-			})
-
-			vim.api.nvim_command("colorscheme catppuccin")
 		end,
 	},
 }

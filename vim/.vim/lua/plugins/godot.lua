ac8781107d02770cb6195ea9c47599fb48758186
Christian Nieves <cnieves@google.com>
2023-12-08 20:34:09 -0600

    meh

--- vim/.vim/lua/plugins/godot.lua
+++ vim/.vim/lua/plugins/godot.lua
@@ -4,20 +4,4 @@ return {
 		"habamax/vim-godot",
 		cond = not use_google(),
 	},
-	{
-		"QuickGD/quickgd.nvim",
-		ft = { "gdshader", "gdshaderinc" },
-		cmd = { "GodotRun", "GodotRunLast", "GodotStart" },
-		cond = not use_google(),
-		-- Use opts if passing in settings else use config
-		init = function()
-			vim.filetype.add({
-				extension = {
-					gdshaderinc = "gdshaderinc",
-				},
-			})
-		end,
-		config = true,
-		opts = {}, -- remove config and use this if changing settings.
-	},
 }

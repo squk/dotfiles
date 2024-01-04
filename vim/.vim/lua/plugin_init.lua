a8e22c563ec89299ebe77b66a0bb72d36672beab
Christian Nieves <cnieves@google.com>
2024-01-04 22:31:50 +0000

    don't force SSH on github URLs

--- vim/.vim/lua/plugin_init.lua
+++ vim/.vim/lua/plugin_init.lua
@@ -5,7 +5,6 @@ if not vim.loop.fs_stat(lazypath) then
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
 		lazypath,
 	})
 end

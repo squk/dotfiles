d2e6748eeb379efe7f721fcfb8fe8866a911af6b
Christian Nieves <cnieves@google.com>
2024-03-10 05:23:23 -0500

    stuff

--- vim/.vim/lua/plugins/themes-personal.lua
+++ vim/.vim/lua/plugins/themes-personal.lua
@@ -12,6 +12,7 @@ return {
 		end,
 	},
 	{
+		"nyoom-engineering/oxocarbon.nvim",
 		lazy = use_google(), -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		cond = false,

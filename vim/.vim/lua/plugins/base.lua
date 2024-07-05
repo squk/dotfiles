1c5740014fbc52c99c55e3cf7ba0e23b3584e84d
Christian Nieves <cnieves@google.com>
2024-07-05 21:39:13 +0000

    auto load session

--- vim/.vim/lua/plugins/base.lua
+++ vim/.vim/lua/plugins/base.lua
@@ -49,11 +49,21 @@ return {
 	{
 		"folke/persistence.nvim",
 		event = "BufReadPre", -- this will only start session saving when an actual file was opened
-		opts = {
-			-- add any custom options here
-		},
+		init = function()
+			vim.api.nvim_create_autocmd("VimEnter", {
+				group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
+				callback = function()
+					if vim.fn.getcwd() ~= vim.env.HOME then
+						require("persistence").load()
+					end
+				end,
+				nested = true,
+			})
+		end,
+		opts = {},
 		keys = {
-			{ "<leader>s", [[<cmd>lua require("persistence").load()<cr>]] },
+			{ "<leader>ss", [[<cmd>lua require("persistence").save()<cr>]] },
+			{ "<leader>sl", [[<cmd>lua require("persistence").load()<cr>]] },
 		},
 	},
 	{

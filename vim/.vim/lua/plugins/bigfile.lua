b296094f425dc9eb5877edcc9462e742d10c1407
Christian Nieves <cnieves@google.com>
2024-11-25 19:27:56 +0000

    derp

--- vim/.vim/lua/plugins/bigfile.lua
+++ vim/.vim/lua/plugins/bigfile.lua
@@ -1,8 +1,9 @@
 return {
-  require("bigfile").setup {
-    filesize = 2,    -- size of the file in MiB, the plugin round file sizes to the closest MiB
+  "LunarVim/bigfile.nvim",
+  opts = {
+    filesize = 2,      -- size of the file in MiB, the plugin round file sizes to the closest MiB
     pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
-    features = {     -- features to disable
+    features = {       -- features to disable
       "indent_blankline",
       "illuminate",
       "lsp",

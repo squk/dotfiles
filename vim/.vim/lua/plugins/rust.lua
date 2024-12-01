111e7dfe233047e94425181f85e6c4b9fab5cfe4
Christian Nieves <cnieves@google.com>
2024-12-01 12:10:19 -0600

    rust change

--- vim/.vim/lua/plugins/rust.lua
+++ vim/.vim/lua/plugins/rust.lua
@@ -6,10 +6,12 @@ return {
 	},
 	{
 		"saecki/crates.nvim",
-		ft = "rust",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		-- event = { "BufRead Cargo.toml" },
+		-- ft = "rust",
+		-- dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("crates").setup({
+
 				completion = {
 					cmp = {
 						enabled = true,

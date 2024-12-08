29bb1af1e00187c1b1b8a390afe2c20e2b3db983
Christian Nieves <cnieves@google.com>
2024-12-07 21:30:03 -0600

    rust formatter

--- vim/.vim/lua/plugins/conform.lua
+++ vim/.vim/lua/plugins/conform.lua
@@ -33,6 +33,7 @@ return {
 		},
 		opts = {
 			formatters_by_ft = {
+				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run multiple formatters sequentially
 				-- go = { "goimports", "gofmt" },
 				-- Use a sub-list to run only the first available formatter

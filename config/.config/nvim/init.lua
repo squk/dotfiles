07c24bdafdb9301910278d8c25066e80d43d8f2d
Christian Nieves <cnieves@google.com>
2024-12-12 17:16:40 +0000

    whatver

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,26 +1,29 @@
-vim.cmd("source " .. vim.fn.expand("$HOME") .. "/.vimrc")
+vim.cmd("source " .. vim.env.HOME .. "/.vimrc")
 
-local use_google = require("utils").use_google
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.HOME .. "/.vim")
+vim.opt.rtp:prepend(vim.env.HOME .. "/.vim/lua")
 
 package.path = package.path .. ";" .. vim.env.HOME .. "/.vim/lua/?.lua"
 
 local plugins = {
-	-- this entry tells lazy.nvim to load the list of of *.lua files from plugins/
-	{ import = "plugins" },
+  -- this entry tells lazy.nvim to load the list of of *.lua files from plugins/
+  { import = "plugins" },
 }
-require("lazy").setup(plugins)
 require("config.clipboard")
+require("config.tmpl")
 require("config.zip")
+
+require("lazy").setup(plugins)
 
 vim.opt.undodir = vim.fn.expand("$HOME") .. "/.undo/"

f815679d89e58811004b56efa7e3a475d49d3904
Christian Nieves <cnieves@google.com>
2025-01-11 20:36:39 -0600

    blink stuff

--- vim/.vim/lua/plugins/lsp.lua
+++ vim/.vim/lua/plugins/lsp.lua
@@ -73,45 +73,6 @@ return {
 			})
 			-- Run gofmt + goimports on save
 
-<<<<<<< HEAD
-      local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.go",
-        callback = function()
-          require("go.format").goimports()
-        end,
-        group = format_sync_grp,
-      })
-    end,
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-      "nvim-lua/lsp-status.nvim",
-      "VonHeikemen/lsp-zero.nvim",
-    },
-    keys = {
-      { "<leader>F",  ":lua vim.lsp.buf.format()<CR>" },
-      { "<leader>rn", ":lua vim.lsp.buf.rename()<CR>" },
-      { "L",          ":lua vim.lsp.buf.hover()<CR>" },
-      { "gr",         ":Telescope lsp_references<CR>" },
-      { "gd",         ":lua vim.lsp.buf.definition()<CR>" },
-      -- { "gd", "<cmd>Telescope lsp_definitions<CR>" },
-      { "gD",         ":tab split | lua vim.lsp.buf.definition()<CR>" },
-      { "gi",         ":lua vim.lsp.buf.implementation()<CR>" },
-      { "gI",         ":lua vim.lsp.buf.implementation()<CR>" },
-      { "gR",         ":lua vim.lsp.buf.references()<CR>" },
-      { "gt",         ":lua vim.lsp.buf.type_definition()<CR>" },
-      { "<C-g>",      ":lua vim.lsp.buf.signature_help()<CR>" },
-      { "<C-g>",      ":lua vim.lsp.buf.signature_help()<CR>",        mode = "i" },
-    },
-    config = function()
-      local lsp_status = require("lsp-status")
-      lsp_status.register_progress()
-=======
 			local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				pattern = "*.go",
@@ -149,7 +110,6 @@ return {
 		config = function()
 			local lsp_status = require("lsp-status")
 			lsp_status.register_progress()
->>>>>>> cc84d4d (i dont even know)
 
 			local capabilities = flags.blink
 					and require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())

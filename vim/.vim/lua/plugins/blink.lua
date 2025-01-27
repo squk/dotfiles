1bbcef640d66dad1bf989b759c51d93513db8ad1
Christian Nieves <cnieves@google.com>
2025-01-27 20:49:29 +0000

    who knows

--- vim/.vim/lua/plugins/blink.lua
+++ vim/.vim/lua/plugins/blink.lua
@@ -2,170 +2,152 @@ local use_google = require("utils").use_google
 local flags = require("utils").flags
 
 return {
-	{
-		"saghen/blink.compat",
-		-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-		version = "*",
-		-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-		lazy = true,
-		cond = flags.blink,
-		-- make sure to set opts so that lazy.nvim calls blink.compat's setup
-		opts = {
-			impersonate_nvim_cmp = true,
-			debug = true,
-		},
-	},
-	{
-		"saghen/blink.cmp",
-		lazy = false, -- lazy loading handled internally
-		cond = flags.blink,
-		dependencies = {
-			"Exafunction/codeium.nvim",
-			"chrisgrieser/cmp-nerdfont",
-			"hrsh7th/cmp-nvim-lsp",
-			"mikavilpas/blink-ripgrep.nvim",
-			"moyiz/blink-emoji.nvim",
-			"rafamadriz/friendly-snippets", -- optional: provides snippets for the snippet source
-			"saghen/blink.compat",
-		},
-		version = "v0.*", -- use a release tag to download pre-built binaries
-		-- build = 'cargo build --release',
+  {
+    "saghen/blink.compat",
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = "*",
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    cond = flags.blink,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {
+      impersonate_nvim_cmp = true,
+      debug = true,
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    lazy = false, -- lazy loading handled internally
+    cond = flags.blink,
+    dependencies = {
+      "Exafunction/codeium.nvim",
+      "chrisgrieser/cmp-nerdfont",
+      "hrsh7th/cmp-nvim-lsp",
+      "mikavilpas/blink-ripgrep.nvim",
+      "moyiz/blink-emoji.nvim",
+      "rafamadriz/friendly-snippets", -- optional: provides snippets for the snippet source
+      "saghen/blink.compat",
+    },
+    version = "v0.*", -- use a release tag to download pre-built binaries
+    -- build = 'cargo build --release',
 
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
       -- stylua: ignore
-			keymap = {
-			  preset = "none",
-				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-				["<C-e>"] = { "hide", "fallback" },
-				["<CR>"] = { "accept", "fallback" },
+      keymap = {
+        preset = "none",
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
 
-				["<Tab>"] = { "select_next", "snippet_forward", "accept", "fallback" },
-				["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<Tab>"] = { "select_next", "snippet_forward", "accept", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 
-				["<Up>"] = { "select_prev", "fallback" },
-				["<Down>"] = { "select_next", "fallback" },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
 
-				["<S-Up>"] = { "scroll_documentation_up", "fallback" },
-				["<S-Down>"] = { "scroll_documentation_down", "fallback" },
-			},
-			sources = {
-				default = function(ctx)
-					local providerToEnable = {
-						"lsp",
-						"path",
-						"snippets",
-						"ripgrep",
-						"emoji",
-						"nerdfont",
-						"buffer",
-					}
-					if use_google() then
-						table.insert(providerToEnable, "nvim_ciderlsp")
-						table.insert(providerToEnable, "buganizer")
-					else
-						table.insert(providerToEnable, "codeium")
-					end
-					return providerToEnable
-				end,
-				-- default = { "lsp" },
-				providers = {
-					lsp = { name = "LSP", module = "blink.cmp.sources.lsp", score_offset = 90 },
-					-- dont show LuaLS require statements when lazydev has items
-					lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", fallbacks = { "lsp" } },
-					ripgrep = {
-						module = "blink-ripgrep",
-						name = "Ripgrep",
-						-- the options below are optional, some default values are shown
-						---@module "blink-ripgrep"
-						---@type blink-ripgrep.Options
-						opts = {
-							prefix_min_len = 3,
-							context_size = 5,
-							max_filesize = "1M",
-							additional_rg_options = {},
-						},
-					},
-					-- https://github.com/moyiz/blink-emoji.nvim
-					emoji = {
-						module = "blink-emoji",
-						name = "Emoji",
-						score_offset = 15, -- the higher the number, the higher the priority
-						opts = { insert = true }, -- Insert emoji (default) or complete its name
-					},
-					buffer = {
-						name = "Buffer",
-						enabled = true,
-						max_items = 3,
-						module = "blink.cmp.sources.buffer",
-						min_keyword_length = 4,
-						score_offset = 15, -- the higher the number, the higher the priority
-					},
-					-- compat sources
-					nerdfont = {
-						name = "nerdfont",
-						module = "blink.compat.source",
-					},
-					codeium = {
-						name = "codeium",
-						module = "blink.compat.source",
-						score_offset = 100,
-					},
-					nvim_ciderlsp = {
-						name = "nvim_ciderlsp",
-						module = "blink.compat.source",
-						score_offset = 100,
-					},
-					buganizer = {
-						name = "nvim_buganizer",
-						module = "blink.compat.source",
-					},
-				},
-			},
-			-- experimental signature help support
-			signature = { enabled = true },
-			completion = {
-				list = {
-				      -- stylua: ignore
-				      selection = {
-				        preselect = function(ctx) return ctx.mode ~= 'cmdline' end,
-				        auto_insert = function(ctx) return ctx.mode ~= 'cmdline' end,
-				      },
-				},
-				documentation = {
-					auto_show = true,
-				},
-				-- Displays a preview of the selected item on the current line
-				ghost_text = {
-					enabled = true,
-				},
-				menu = {
-					draw = {
-						components = {
-							kind_icon = {
-								ellipsis = false,
-								text = function(ctx)
-									local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-									return kind_icon
-								end,
-								-- Optionally, you may also use the highlights from mini.icons
-								highlight = function(ctx)
-									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-									return hl
-								end,
-							},
-						},
-					},
-				},
-				trigger = {
-					show_on_x_blocked_trigger_characters = { "'", '"', "(", "{" },
-				},
-			},
-		},
+        ["<S-Up>"] = { "scroll_documentation_up", "fallback" },
+        ["<S-Down>"] = { "scroll_documentation_down", "fallback" },
+      },
+      sources = {
+        default = function(ctx)
+          local providerToEnable = {
+            "lsp",
+            "path",
+            "snippets",
+            "ripgrep",
+            "emoji",
+            "nerdfont",
+            "buffer",
+          }
+          if use_google() then
+            table.insert(providerToEnable, "nvim_ciderlsp")
+            table.insert(providerToEnable, "buganizer")
+          else
+            table.insert(providerToEnable, "codeium")
+          end
+          return providerToEnable
+        end,
+        -- default = { "lsp" },
+        providers = {
+          lsp = { name = "LSP", module = "blink.cmp.sources.lsp", score_offset = 90 },
+          -- dont show LuaLS require statements when lazydev has items
+          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", fallbacks = { "lsp" } },
+          ripgrep = {
+            module = "blink-ripgrep",
+            name = "Ripgrep",
+            -- the options below are optional, some default values are shown
+            ---@module "blink-ripgrep"
+            ---@type blink-ripgrep.Options
+            opts = {
+              prefix_min_len = 3,
+              context_size = 5,
+              max_filesize = "1M",
+              additional_rg_options = {},
+            },
+          },
+          -- https://github.com/moyiz/blink-emoji.nvim
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+            score_offset = 15,  -- the higher the number, the higher the priority
+            opts = { insert = true }, -- Insert emoji (default) or complete its name
+          },
+          buffer = {
+            name = "Buffer",
+            enabled = true,
+            max_items = 3,
+            module = "blink.cmp.sources.buffer",
+            min_keyword_length = 4,
+            score_offset = 15, -- the higher the number, the higher the priority
+          },
+          -- compat sources
+          nerdfont = {
+            name = "nerdfont",
+            module = "blink.compat.source",
+          },
+          codeium = {
+            name = "codeium",
+            module = "blink.compat.source",
+            score_offset = 100,
+          },
+          nvim_ciderlsp = {
+            name = "nvim_ciderlsp",
+            module = "blink.compat.source",
+            score_offset = 100,
+          },
+          buganizer = {
+            name = "nvim_buganizer",
+            module = "blink.compat.source",
+          },
+        },
+      },
+      -- experimental signature help support
+      signature = { enabled = true },
+      completion = {
+        list = {
+          -- stylua: ignore
+          selection = {
+            preselect = function(ctx) return ctx.mode ~= 'cmdline' end,
+            auto_insert = function(ctx) return ctx.mode ~= 'cmdline' end,
+          },
+        },
+        documentation = {
+          auto_show = true,
+        },
+        -- Displays a preview of the selected item on the current line
+        ghost_text = {
+          enabled = true,
+        },
+        trigger = {
+          show_on_x_blocked_trigger_characters = { "'", '"', "(", "{" },
+        },
+      },
+    },
 
-		-- allows extending the providers array elsewhere in your config
-		-- without having to redefine it
-		opts_extend = { "sources.default" },
-	},
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.default" },
+  },
 }
--- vim/.vim/lua/utils.lua
+++ vim/.vim/lua/utils.lua
@@ -1,100 +1,100 @@
 local M = {
-	use_google_cache = nil,
-	flags = { blink = false },
+  use_google_cache = nil,
+  flags = { blink = true },
 }
 
 function M.exec(command, args)
-	local Job = require("plenary.job")
-	local job = Job:new({
-		command = command,
-		args = args,
-	})
-	job:sync()
-	job:wait()
-	return job:result()
+  local Job = require("plenary.job")
+  local job = Job:new({
+    command = command,
+    args = args,
+  })
+  job:sync()
+  job:wait()
+  return job:result()
 end
 
 function M.map(mode, lhs, rhs, opts)
-	local options = { noremap = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	-- vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-	vim.keymap.set(mode, lhs, rhs, options)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  -- vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
 function M.use_google()
-	if M.use_google_cache == nil then
-		M.use_google_cache = M.file_exists(os.getenv("HOME") .. "/use_google")
-	end
-	return M.use_google_cache
+  if M.use_google_cache == nil then
+    M.use_google_cache = M.file_exists(os.getenv("HOME") .. "/use_google")
+  end
+  return M.use_google_cache
 end
 
 function M.file_exists(name)
-	local f = io.open(name, "r")
-	if f ~= nil then
-		io.close(f)
-		return true
-	else
-		return false
-	end
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
 end
 
 function M.dump(o)
-	if type(o) == "table" then
-		local s = "{ "
-		for k, v in pairs(o) do
-			if type(k) ~= "number" then
-				k = '"' .. k .. '"'
-			end
-			s = s .. "[" .. k .. "] = " .. M.dump(v) .. ","
-		end
-		return s .. "} "
-	else
-		return tostring(o)
-	end
+  if type(o) == "table" then
+    local s = "{ "
+    for k, v in pairs(o) do
+      if type(k) ~= "number" then
+        k = '"' .. k .. '"'
+      end
+      s = s .. "[" .. k .. "] = " .. M.dump(v) .. ","
+    end
+    return s .. "} "
+  else
+    return tostring(o)
+  end
 end
 
 function M.tprint(tbl, indent)
-	if not indent then
-		indent = 0
-	end
-	local toprint = string.rep(" ", indent) .. "{\r\n"
-	indent = indent + 2
-	for k, v in pairs(tbl) do
-		toprint = toprint .. string.rep(" ", indent)
-		if type(k) == "number" then
-			toprint = toprint .. "[" .. k .. "] = "
-		elseif type(k) == "string" then
-			toprint = toprint .. k .. "= "
-		end
-		if type(v) == "number" then
-			toprint = toprint .. v .. ",\r\n"
-		elseif type(v) == "string" then
-			toprint = toprint .. '"' .. v .. '",\r\n'
-		elseif type(v) == "table" then
-			toprint = toprint .. M.tprint(v, indent + 2) .. ",\r\n"
-		else
-			toprint = toprint .. '"' .. tostring(v) .. '",\r\n'
-		end
-	end
-	toprint = toprint .. string.rep(" ", indent - 2) .. "}"
-	return toprint
+  if not indent then
+    indent = 0
+  end
+  local toprint = string.rep(" ", indent) .. "{\r\n"
+  indent = indent + 2
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if type(k) == "number" then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif type(k) == "string" then
+      toprint = toprint .. k .. "= "
+    end
+    if type(v) == "number" then
+      toprint = toprint .. v .. ",\r\n"
+    elseif type(v) == "string" then
+      toprint = toprint .. '"' .. v .. '",\r\n'
+    elseif type(v) == "table" then
+      toprint = toprint .. M.tprint(v, indent + 2) .. ",\r\n"
+    else
+      toprint = toprint .. '"' .. tostring(v) .. '",\r\n'
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent - 2) .. "}"
+  return toprint
 end
 
 function M.log(message)
-	local log_file_path = vim.fn.expand("$HOME/nvim.log")
-	local log_file = io.open(log_file_path, "a")
-	io.output(log_file)
-	io.write(message .. "\n")
-	io.close(log_file)
+  local log_file_path = vim.fn.expand("$HOME/nvim.log")
+  local log_file = io.open(log_file_path, "a")
+  io.output(log_file)
+  io.write(message .. "\n")
+  io.close(log_file)
 end
 
 function M.TableConcat(t1, t2)
-	for i = 1, #t2 do
-		t1[#t1 + 1] = t2[i]
-	end
-	return t1
+  for i = 1, #t2 do
+    t1[#t1 + 1] = t2[i]
+  end
+  return t1
 end
 
 return M

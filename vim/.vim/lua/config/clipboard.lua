f595b1fcfaa14785e9cbb32f7a94ca02c4a62025
Christian Nieves <cnieves@google.com>
2025-03-07 13:55:29 -0600

    wayland clipboard support

--- vim/.vim/lua/config/clipboard.lua
+++ vim/.vim/lua/config/clipboard.lua
@@ -1,18 +1,33 @@
 local map = require("utils").map
 
-if vim.env.SSH_TTY or vim.env.SSH_CLIENT or vim.env.SSH_CONNECTION then
+if vim.fn.hostname() == "goblin" then
 	vim.g.clipboard = {
-		name = "lemonade",
+		name = "wayland",
 		copy = {
-			["+"] = { "lemonade", "copy" },
-			["*"] = { "lemonade", "copy" },
+			["+"] = { "wl-copy" },
+			["*"] = { "wl-copy" },
 		},
 		paste = {
-			["+"] = { "lemonade", "paste" },
-			["*"] = { "lemonade", "paste" },
+			["+"] = { "wl-paste" },
+			["*"] = { "wl-paste" },
 		},
 		cache_enabled = false,
 	}
+else
+  if vim.env.SSH_TTY or vim.env.SSH_CLIENT or vim.env.SSH_CONNECTION then
+	  vim.g.clipboard = {
+		  name = "lemonade",
+		  copy = {
+			  ["+"] = { "lemonade", "copy" },
+			  ["*"] = { "lemonade", "copy" },
+		  },
+		  paste = {
+			  ["+"] = { "lemonade", "paste" },
+			  ["*"] = { "lemonade", "paste" },
+		  },
+		  cache_enabled = false,
+	  }
+  end
 end
 
 map("v", "<leader>y", '"+y')
